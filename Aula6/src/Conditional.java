c63d811b45b0b179abf46550e168b42c5e42fac8
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2019-03-08 05:54:06 -0300

    Add aula_6

---
+++ Aula6/src/Conditional.java
@@ -0,0 +1,56 @@
+
+public class Conditional {
+
+	public static void main(String[] arg) {
+		int price = 103;
+		
+		if (price >= 1 && price <= 5) {
+			System.out.println("Está muito barato!");
+		} 
+		else if (price >= 6 && price <= 20) {
+			System.out.println("Preço normal!");
+		} 
+		else  {
+			System.out.println("Está muito caro!");
+		}
+		
+		
+		int a = 21;
+		int b = 19;
+		int c = 19;
+		
+		if (a <= b && a <= c) {
+			System.out.println("A é a menor: " + a);
+		
+		} else if (b <= a && b <= c) {
+			System.out.println("B é a menor: " + b);
+		
+		} else {
+			System.out.print("C é a menor: " + c);
+		}
+		
+		
+		
+		
+		int age = 17;
+		//comentario de linha!
+		if (age == 16 || age == 17) {
+			System.out.println("Você já pode votar!");
+		}
+		
+		/*
+		 comentario de "N" linhas!
+		 */
+		
+		if (a >= b && b >= c) {
+			System.out.println(a + " " + b + " " + c);
+		} else if (b >= a && a >= c) {
+			System.out.println(b + " " + a + " " + c);
+		} else if (c >= a && a >= b) {
+			System.out.println(c + " " + a + " " + b);
+		}
+		
+		
+	}
+	
+}
