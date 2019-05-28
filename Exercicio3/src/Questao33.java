16eb4fe0d249d0ff6b817082f56f45e7dc704b9e
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2019-05-28 05:53:09 -0300

    Add exercicio correction

--- Exercicio3/src/Questao33.java
+++ Exercicio3/src/Questao33.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Questao33 {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
 		int[] numeros = new int[50];
@@ -26,6 +26,33 @@ public class Questao33 {
 				System.out.println(numeros[i]);
 			}
 		}
-	}
+	}*/
 	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int max = 5;
+		int[] numeros = new int[max];
+		int[] numerosRepetidos = new int[max];
+				
+		for (int i = 0; i < max; i++) {
+			System.out.println("Digite um número:");
+			numeros[i] = scanner.nextInt();
+		}
+		
+		for (int i = 0; i < max - 1; i++) {
+			for (int j = i + 1; j < max; j++) {
+				if (numeros[i] == numeros[j]) {
+					numerosRepetidos[i] = numeros[i];
+					numerosRepetidos[j] = numeros[j];
+				}
+			}
+		}
+		
+		for (int i = 0; i < max; i++) {
+			if (numerosRepetidos[i] != 0) {
+				System.out.println(numerosRepetidos[i]);
+			}
+		}
+	}
 }
